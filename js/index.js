9bdb809bb40d421e8172ee616039afbd78719ac8
ZERO34802 <karthikp9@gmail.com>
2023-09-22 15:22:23 +0530

    Javascript Practice
    Javascript Practice file added

---
+++ js/index.js
@@ -0,0 +1,11 @@
+//Normal Function declaration
+
+function getName() {
+  console.log("My Name is Karthik");
+}
+
+//Arrow Function Declaration
+
+var getName = () => {
+  console.log("My Name is Karthik");
+};
