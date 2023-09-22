184e2a4e7dece58aee980b65a7217a6388100c36
ZERO34802 <karthikp9@gmail.com>
2023-09-22 16:07:19 +0530

    Javascript Practice
    Changes made to Javascript Practice

--- js/index.js
+++ js/index.js
@@ -5,7 +5,27 @@ function getName() {
 }
 
 //Arrow Function Declaration
-
+//Function keyword is not used
+//function name is assigned to a variable
 var getName = () => {
   console.log("My Name is Karthik");
 };
+
+//Understanding execution context and var scope
+var x = 1;
+if (true) {
+  var x = 20;
+}
+a();
+b();
+console.log(x);
+
+function a() {
+  var x = 10;
+  console.log(x);
+}
+
+function b() {
+  var x = 100;
+  console.log(x);
+}
