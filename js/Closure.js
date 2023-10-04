56ab984bdc549a072e35504282c592c48e7facfc
ZERO34802 <99256532+ZERO34802@users.noreply.github.com>
2023-10-04 16:19:07 +0530

    Create Closure.js

---
+++ js/Closure.js
@@ -0,0 +1,4 @@
+//What is a closure?
+//A function bundled together with it's lexical environment is known as closure.
+//Whenever function is returned, even if its vanished in execution context but
+// still it remembers the reference it was pointing to
